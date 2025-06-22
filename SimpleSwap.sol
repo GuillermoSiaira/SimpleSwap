62619fae8c21e73543710857c284faed2a6d1932
Tu Nombre <tu_email@ejemplo.com>
2025-06-22 20:12:25 -0300

    Add SimpleSwap contract with NatSpec, README, and MockSwapVerifier

--- SimpleSwap.sol
+++ SimpleSwap.sol
@@ -24,14 +24,25 @@ interface ISimpleSwapVerifier {
 contract SimpleSwap {
     using SafeMath for uint;
 
+    /**
+     * @notice The address of the verifier contract used for liquidity and swap operations.
+     */
     address public verifier;
 
+    /**
+     * @notice Initializes the SimpleSwap contract with the address of the verifier contract.
+     * @param _verifierAddress The address of the verifier contract used for liquidity and swap operations.
+     */
     constructor(address _verifierAddress) {
         verifier = _verifierAddress;
     }
 
     /**
-     * @notice Calculates the optimal amount of one token based on the other.
+     * @notice Calculates the amount of tokenB required to match the value of a given amount of tokenA based on pool reserves.
+     * @param amountA The amount of tokenA to be provided.
+     * @param reserveA The current reserve of tokenA in the pool.
+     * @param reserveB The current reserve of tokenB in the pool.
+     * @return amountB The calculated amount of tokenB proportional to amountA.
      */
     function _quote(uint amountA, uint reserveA, uint reserveB) internal pure returns (uint amountB) {
         require(amountA > 0, "SimpleSwap: INSUFFICIENT_AMOUNT");
@@ -40,7 +51,12 @@ contract SimpleSwap {
     }
     
     /**
-     * @notice Calculates the output amount for a given input amount and reserves.
+     * @notice Calculates the amount of output tokens received for a given input amount and pool reserves.
+     * @dev Applies a 0.3% fee (997/1000) to the input amount as per the constant product formula.
+     * @param amountIn The amount of input tokens to be swapped.
+     * @param reserveIn The current reserve of the input token in the pool.
+     * @param reserveOut The current reserve of the output token in the pool.
+     * @return amountOut The calculated amount of output tokens to be received.
      */
     function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) public pure returns (uint amountOut) {
         require(amountIn > 0, "SimpleSwap: INSUFFICIENT_INPUT_AMOUNT");
@@ -53,7 +69,19 @@ contract SimpleSwap {
     }
 
     /**
-     * @notice Adds liquidity to an ERC-20 pair pool.
+     * @notice Adds liquidity to an ERC-20 token pair pool.
+     * @dev Calculates optimal token amounts based on pool reserves and mints LP tokens to the recipient. If the pool is empty, uses desired amounts; otherwise, ensures proportional deposits.
+     * @param tokenA The address of the first token in the pair.
+     * @param tokenB The address of the second token in the pair.
+     * @param amountADesired The desired amount of tokenA to add.
+     * @param amountBDesired The desired amount of tokenB to add.
+     * @param amountAMin The minimum acceptable amount of tokenA to add.
+     * @param amountBMin The minimum acceptable amount of tokenB to add.
+     * @param to The address that will receive the LP tokens.
+     * @param deadline The timestamp by which the transaction must be mined.
+     * @return amountA The actual amount of tokenA added to the pool.
+     * @return amountB The actual amount of tokenB added to the pool.
+     * @return liquidity The amount of LP tokens minted.
      */
     function addLiquidity(
         address tokenA,
@@ -98,8 +126,6 @@ contract SimpleSwap {
         address pair = ISimpleSwapVerifier(verifier).getPair(tokenA, tokenB);
         require(pair != address(0), "SimpleSwap: PAIR_NOT_FOUND");
         
-        // This logic assumes the LP tokens are minted to this contract.
-        // It then transfers them to the final recipient `to`.
         uint lpBalance = IERC20(pair).balanceOf(address(this));
         if (lpBalance > 0) {
            IERC20(pair).transfer(to, lpBalance);
@@ -107,7 +133,17 @@ contract SimpleSwap {
     }
 
     /**
-     * @notice Removes liquidity from a pool.
+     * @notice Removes liquidity from an ERC-20 token pair pool.
+     * @dev Burns LP tokens and transfers the corresponding amounts of tokenA and tokenB to the recipient.
+     * @param tokenA The address of the first token in the pair.
+     * @param tokenB The address of the second token in the pair.
+     * @param liquidity The amount of LP tokens to burn.
+     * @param amountAMin The minimum acceptable amount of tokenA to receive.
+     * @param amountBMin The minimum acceptable amount of tokenB to receive.
+     * @param to The address that will receive the tokens.
+     * @param deadline The timestamp by which the transaction must be mined.
+     * @return amountA The amount of tokenA received.
+     * @return amountB The amount of tokenB received.
      */
     function removeLiquidity(
         address tokenA,
@@ -137,6 +173,13 @@ contract SimpleSwap {
 
     /**
      * @notice Swaps an exact amount of input tokens for as many output tokens as possible.
+     * @dev Supports direct swaps (single token pair). The path array must contain exactly two token addresses.
+     * @param amountIn The amount of input tokens to swap.
+     * @param amountOutMin The minimum acceptable amount of output tokens to receive.
+     * @param path An array of token addresses representing the swap path (input token, output token).
+     * @param to The address that will receive the output tokens.
+     * @param deadline The timestamp by which the transaction must be mined.
+     * @return amounts An array containing the input amount and the output amount.
      */
     function swapExactTokensForTokens(
         uint amountIn,
@@ -154,7 +197,7 @@ contract SimpleSwap {
         IERC20(path[0]).transferFrom(msg.sender, address(this), amountIn);
         IERC20(path[0]).approve(verifier, amountIn);
         
-        ISimpleSwapVerifier(verifier).swap(path[0], amountIn, path[1], amountOutMin);
+        ISSimpleSwapVerifier(verifier).swap(path[0], amountIn, path[1], amountOutMin);
 
         uint amountOut = IERC20(path[1]).balanceOf(address(this));
         require(amountOut >= amountOutMin, "SimpleSwap: INSUFFICIENT_OUTPUT_AMOUNT");
@@ -164,7 +207,11 @@ contract SimpleSwap {
     }
 
     /**
-     * @notice Gets the price of tokenA in terms of tokenB.
+     * @notice Calculates the price of tokenA in terms of tokenB based on pool reserves.
+     * @dev Returns the price with 18 decimals of precision (multiplied by 1e18).
+     * @param tokenA The address of the token to price.
+     * @param tokenB The address of the token in which the price is denominated.
+     * @return price The price of tokenA in terms of tokenB.
      */
     function getPrice(address tokenA, address tokenB) external view returns (uint price) {
         (uint reserveA, uint reserveB) = ISimpleSwapVerifier(verifier).getReserves(tokenA, tokenB);
